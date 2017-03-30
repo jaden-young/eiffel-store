420ccf96ed43553236605ae5d1cd5514acb56e1c
Joakim Argillander <joaar286@student.liu.se>
2017-03-30 15:55:58 +0200

    Merged graph.js

--- imports/api/events/methods.js
+++ imports/api/events/methods.js
@@ -32,13 +32,45 @@ export const getAggregatedGraph = new ValidatedMethod({
         let nodes = [];
         let groupedEvents = _.groupBy(events, (event) => event.data.customData[0].value);
         _.each(groupedEvents, (events, group) => {
-            nodes.push({
-                data: {
-                    id: group,
-                    label: group,
-                    events: events
-                }
-            });
+            if (group.startsWith("TCF") || group.startsWith("TSF")) {
+                nodes.push({
+                    data: {
+                        id: group,
+                        events: events,
+                        length: _.size(events),
+                        passed: _.reduce(events, function(memo, event){
+                            return event.data.outcome.verdict === "PASSED" ? memo + (1/this) : memo;
+                        }, 0, _.size(events)) // Calculating rate of passed tests
+                    }
+                });
+            }
+            else if(group.startsWith("CLM")){
+                nodes.push({
+                    data: {
+                        id: group,
+                        events: events,
+                        length: _.size(events),
+                        passed: _.reduce(events, function (memo, event) {
+                            return event.data.value === "SUCCESS" ? memo + 1 : memo;
+                        }, 0), // Calculating number of passed tests
+                        failed: _.reduce(events, function (memo, event) {
+                            return event.data.value === "FAILURE" ? memo + 1 : memo;
+                        }, 0),
+                        inconclusive: _.reduce(events, function (memo, event) {
+                            return event.data.value === "INCONCLUSIVE" ? memo + 1 : memo;
+                        }, 0)
+                    }
+                });
+            }
+            else {
+                nodes.push({
+                    data: {
+                        id: group,
+                        label: group,
+                        events: events
+                    }
+                });
+            }
 
             // Save the links from events -> group and group -> events to reconstruct group -> group later
             let links = _.reduce(events, (memo, event) => memo.concat(event.links), []);
@@ -60,37 +92,37 @@ export const getAggregatedGraph = new ValidatedMethod({
 
 
 export const getEventAncestorGraph = new ValidatedMethod({
-   name:'getEventAncestorGraph',
+    name:'getEventAncestorGraph',
     validate: null,
     run({ eventId }) {
-       let emptyGraph = {nodes: {}, edges: {}};
-       if (Meteor.isClient) {
-           return emptyGraph;
-       }
+        let emptyGraph = {nodes: {}, edges: {}};
+        if (Meteor.isClient) {
+            return emptyGraph;
+        }
 
-       if (Meteor.isServer) {
-           let createAncestorGraph = function (graph, eventId) {
-               let event = Events.findOne({'meta.id': eventId});
-               let parentIds = _.map(event.links, (link) => link.target);
+        if (Meteor.isServer) {
+            let createAncestorGraph = function (graph, eventId) {
+                let event = Events.findOne({'meta.id': eventId});
+                let parentIds = _.map(event.links, (link) => link.target);
 
-               // Save nodes in a map to prevent duplicates
-               graph.nodes[eventId] = ({data: {id: event.meta.id, label: event.meta.type}});
+                // Save nodes in a map to prevent duplicates
+                graph.nodes[eventId] = ({data: {id: event.meta.id, label: event.meta.type}});
 
-               // Save edges in a map to prevent duplicates
-               _.each(parentIds, (parentId) => {
-                   let id = eventId + ':' + parentId;
-                   graph.edges[id] = {
-                       data: {
-                           source: eventId,
-                           target: parentId
-                       }
-                   }
-               });
-               return _.reduce(parentIds, createAncestorGraph, graph);
-           };
+                // Save edges in a map to prevent duplicates
+                _.each(parentIds, (parentId) => {
+                    let id = eventId + ':' + parentId;
+                    graph.edges[id] = {
+                        data: {
+                            source: eventId,
+                            target: parentId
+                        }
+                    }
+                });
+                return _.reduce(parentIds, createAncestorGraph, graph);
+            };
 
-           let graph = createAncestorGraph(emptyGraph, eventId);
-           return { nodes: _.values(graph.nodes), edges: _.values(graph.edges) };
-       }
+            let graph = createAncestorGraph(emptyGraph, eventId);
+            return { nodes: _.values(graph.nodes), edges: _.values(graph.edges) };
+        }
     }
 });
