4c887915d77f669beb12cc41814487f4c3398ce1
victorbodin <vicbo282@student.liu.se>
2017-04-10 14:57:37 +0200

    Timeline working with date input boxes

--- imports/ui/components/aggregation.js
+++ imports/ui/components/aggregation.js
@@ -21,7 +21,7 @@ Template.aggregation.rendered = () => {
             datepickers = $('.datepicker'),
             defaultLimit = 500,
             defaultFrom = '01/02/2012',
-            defaultTo = '01/02/2018';
+            defaultTo = '01/02/2017';
 
         // Set default input values
         fromInput.val(defaultFrom);
@@ -34,11 +34,43 @@ Template.aggregation.rendered = () => {
             changeYear: true
         });
 
+        /* TEST TIMELINE */
+    // DOM element where the Timeline will be attached
+        let container = document.getElementById('example-timeline');
+    // Create a DataSet with data
+        let data = new vis.DataSet([{
+            id: 1,
+            content: 'Lower limit',
+            start: defaultFrom
+        }, {
+            id: 2,
+            content: 'Upper limit',
+            start: defaultTo
+        }]);
+    // Configuration for the Timeline as JSON object
+        let options = {
+            width: '70%',
+            height: '150px',
+            max: new Date(Date.now()).toLocaleDateString(),
+            align: 'center'
+        };
+    // Create a Timeline
+        let timeline = new vis.Timeline(container, data, options);
+        /*---------------*/
+
         let onChange = () => {
             let from = Date.parse(fromInput.val()),
                 to = Date.parse(toInput.val()),
                 limit = parseInt(limitInput.val());
-
+            timeline.setItems( new vis.DataSet([{
+                id: 1,
+                content: 'Lower limit',
+                start: fromInput.val()
+            }, {
+                id: 2,
+                content: 'Upper limit',
+                start: toInput.val()
+            }]));
             showAggregation(from, to, limit);
         };
 
@@ -54,41 +86,7 @@ Template.aggregation.rendered = () => {
 function showAggregation(from, to, limit) {
     console.log('rendering aggregation');
 
-    /* TEST TIMELINE */
-    //Timline demo created using vis.js - http://visjs.org/
 
-//Configuration options for a timeline - http://visjs.org/docs/timeline.html#Configuration_Options
-
-
-
-// DOM element where the Timeline will be attached
-    let container = document.getElementById('example-timeline');
-    console.log('hej '+Date.parse(from.val()));
-// Create a DataSet with data
-    let data = new vis.DataSet([{
-        id: 1,
-        content: 'Lower limit',
-        start: from.toString()
-    }, {
-        id: 2,
-        content: 'Upper limit',
-        start: to.toString()
-    }]);
-
-// Configuration for the Timeline as JSON object
-    let options = {
-        width: '70%',
-        editable: true, /* this option means you can add or remove items by clicking on the timeline */
-        margin: {
-            item: 20
-        }
-    };
-
-// Create a Timeline
-    let timeline = new vis.Timeline(container, data, options);
-
-
-    /*---------------*/
 
     getAggregatedGraph.call({from: from, to: to, limit: limit}, function (error, graph) {
         if (error) {
