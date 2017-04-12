6bfa3c1abe62a4cf95b9f9620b742474e4a4e3eb
Jonathan Wahlund <jonwa732@student.liu.se>
2017-04-12 18:43:36 +0200

    Revert "Merge branch 'feature_start_end_aggregation' into 'development'"
    This reverts merge request !27

--- imports/ui/layout/layout.test.js
+++ imports/ui/layout/layout.test.js
@@ -2,3 +2,12 @@ import { Meteor } from 'meteor/meteor';
 import { assert } from 'meteor/practicalmeteor:chai';
 import { faker } from 'meteor/practicalmeteor:faker';
 import { resetDatabase } from 'meteor/xolvio:cleaner';
+
+if(Meteor.isClient){
+    describe('my module', function () {
+        it('shows the aggregated level', function () {
+            assert(true == false);
+        })
+    })
+
+}
