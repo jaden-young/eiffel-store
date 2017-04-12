910150a6fdf2e3a70cc4d6fcd53a5846a1a86505
Jonathan Wahlund <jonwa732@student.liu.se>
2017-04-12 21:53:32 +0200

    Tooltip button now brings you down to level 2 and have access to the id of which the data for the table should be fetched.

--- imports/ui/components/details.js
+++ imports/ui/components/details.js
@@ -1,16 +1,18 @@
 import {Template} from "meteor/templating";
 import "./details.html";
 
-require( 'datatables.net-bs' )( window, $ );
-import {getTableData} from "./table.js";
+require('datatables.net-bs')(window, $);
 
-$(document).ready( function () {
+$(document).ready(function () {
     $('#table_id').DataTable();
-} );
-
-Template.aggregation.events({
-    'click .tt_button': function(e){
-        console.log("clicked");
-    }
 });
 
+Template.aggregation.events({
+    'click .aggregation-tt-btn': function (event) {
+        console.log(event.target.value);
+
+        $('html, body').animate({
+            scrollTop: $("#details-container").offset().top - 10
+        }, "slow");
+    }
+});
