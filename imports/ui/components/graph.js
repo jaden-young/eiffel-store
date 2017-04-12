96d34e4017329f868874fd0c2d17139b1dc5e900
becczor <rebecca.lindblom@gmail.com>
2017-04-12 13:52:19 +0200

    Total no of events added to all nodes tooltip

--- imports/ui/components/graph.js
+++ imports/ui/components/graph.js
@@ -139,25 +139,26 @@ function renderGraph(graph, container) {
             case /TSF/.test(node_id):                                              // Checks if node_id starts with 'TSF'
                 console.log('CASE', 'TSF');
                 return '<h4>' + node_id  + '</h4>' +           // Tooltip-header (Node-ID)
-                    '<button type="button" class="btn btn-default btn-block tt_button"> Show details </button>' +          // Button will take user to level 2 - 'details'
+                    '<button type="button" class="btn btn-info btn-block tt_button"> Show details </button>' +          // Button will take user to level 2 - 'details'
                     '<table class="table table-bordered">' +
                     '<tr><th>Status</th><th colspan="2">No. of</th></tr>' +    // Table-header
                     '<tr class="success"><td>Passed</td><td class="td-right">' + node_data.passed + '</td><td class="td-right">' + Math.floor(node_data.passed/node_data.length*100) +'%</td></tr>' +
                     '<tr class="danger"><td>Failed</td><td class="td-right">' + node_data.failed + '</td><td class="td-right">' + Math.floor(node_data.failed/node_data.length*100) +'%</td></tr>' +
-                    '<tr><td>Inconclusive</td><td class="td-right">' + node_data.inconclusive + '</td><td class="td-right">' + Math.floor(node_data.inconclusive/node_data.length) +'%</td></tr></table>'; // Row 3 - OTHER
+                    '<tr><td>Inconclusive</td><td class="td-right">' + node_data.inconclusive + '</td><td class="td-right">' + Math.floor(node_data.inconclusive/node_data.length) +'%</td></tr>' +
+                    '<tr><td>Total no. of events</td><td colspan="2" class="td-right">' + node_data.length + '</td></tr>' +
+                    '</table>'; // Row 3 - OTHER
 
             case /TCF/.test(node_id):                                              // Checks if node_id starts with 'TSF'
                 console.log('CASE', 'TCF');
                 return '<h4>' + node_id  + '</h4>' +           // Tooltip-header (Node-ID)
-                    '<button type="button" class="btn btn-default btn-block tt_button">Show all events</button>' +          // Button will take user to level 2 - 'details'
+                    '<button type="button" class="btn btn-info btn-block tt_button">Show all events</button>' +          // Button will take user to level 2 - 'details'
                     '<table class="table table-bordered">' +
-                    '<thead>' +
                     '<tr><th>Status</th><th colspan="2">No. of</th></tr>' +    // Table-header
-                    '</thead><tbody>' +
-                    '<tr><td>Passed</td><td class="td-right">' + node_data.passed + '</td><td class="td-right">' + Math.floor(node_data.passed/node_data.length*100) +'%</td></tr>' +
-                    '<tr><td>Failed</td><td class="td-right">' + node_data.failed + '</td><td class="td-right">' + Math.floor(node_data.failed/node_data.length*100) +'%</td></tr>' +
+                    '<tr class="success"><td>Passed</td><td class="td-right">' + node_data.passed + '</td><td class="td-right">' + Math.floor(node_data.passed/node_data.length*100) +'%</td></tr>' +
+                    '<tr class="danger"><td>Failed</td><td class="td-right">' + node_data.failed + '</td><td class="td-right">' + Math.floor(node_data.failed/node_data.length*100) +'%</td></tr>' +
                     '<tr><td>Inconclusive</td><td class="td-right">' + node_data.inconclusive + '</td><td class="td-right">' + Math.floor(node_data.inconclusive/node_data.length) +'%</td></tr>' +
-                    '</tbody></table>'; // Row 3 - OTHER
+                    '<tr><td>Total no. of events</td><td colspan="2" class="td-right">' + node_data.length + '</td></tr>' +
+                    '</table>'; // Row 3 - OTHER
 
             case /CLM/.test(node_id):
                 console.log('CASE', 'CLM');
@@ -166,15 +167,17 @@ function renderGraph(graph, container) {
                     '<table class="table table-bordered">' +
                     '<tr><td colspan="3"><em>' + node_data.name + '</em></td></tr>' +
                     '<tr><th>Status</th><th colspan="2">No. of</th></tr>' + // table header
-                    '<tr><td>Passed</td><td class="td-right">' + node_data.passed + '</td><td class="td-right">' + Math.floor(node_data.passed/node_data.length*100) +'%</td></tr>' +
-                    '<tr><td>Failed</td><td class="td-right">' + node_data.failed + '</td><td class="td-right">' + Math.floor(node_data.failed/node_data.length*100) +'%</td></tr>' +
-                    '<tr><td>Inconclusive</td><td class="td-right">' + node_data.inconclusive + '</td><td class="td-right">' + Math.floor(node_data.inconclusive/node_data.length) +'%</td></tr></table>';
+                    '<tr class="success"><td>Passed</td><td class="td-right">' + node_data.passed + '</td><td class="td-right">' + Math.floor(node_data.passed/node_data.length*100) +'%</td></tr>' +
+                    '<tr class="danger"><td>Failed</td><td class="td-right">' + node_data.failed + '</td><td class="td-right">' + Math.floor(node_data.failed/node_data.length*100) +'%</td></tr>' +
+                    '<tr><td>Inconclusive</td><td class="td-right">' + node_data.inconclusive + '</td><td class="td-right">' + Math.floor(node_data.inconclusive/node_data.length) +'%</td></tr>' +
+                    '<tr><td>Total no. of events</td><td colspan="2" class="td-right">' + node_data.length + '</td></tr>' +
+                    '</table>';
             default:
                 console.log('Default');
                 return '<h4 id="tt_header">' + node_id  + '</h4>' +
-                '<button type="button" class="btn btn-default btn-block tt_button"> Show all events </button>' +
+                '<button type="button" class="btn btn-info btn-block tt_button"> Show all events </button>' +
                 '<table class="table table-bordered">' +
-                '<tr><td>No of events</td><td class="td-right">' + node_data.length + '</td></tr>' +
+                '<tr><td>Total no. of events</td><td class="td-right">' + node_data.length + '</td></tr>' +
                 '</table>';
 
 
