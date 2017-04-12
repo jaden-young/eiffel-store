74d80cd4e9be70d7ac62391c692b5e749d7c9e81
Jonathan Wahlund <jonwa732@student.liu.se>
2017-04-12 18:26:09 +0200

    Added new structure for level2. Changed styles.

--- imports/ui/components/details.js
+++ imports/ui/components/details.js
@@ -1,10 +1,16 @@
+import {Template} from "meteor/templating";
 import "./details.html";
 
 require( 'datatables.net-bs' )( window, $ );
+import {getTableData} from "./table.js";
 
 $(document).ready( function () {
     $('#table_id').DataTable();
 } );
 
-
+Template.aggregation.events({
+    'click .tt_button': function(e){
+        console.log("clicked");
+    }
+});
 
