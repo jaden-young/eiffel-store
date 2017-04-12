26b5bd701add8b2f9de58bdb5aa583cec18af2a1
Jonathan Wahlund <jonwa732@student.liu.se>
2017-04-12 18:59:14 +0200

    Git fix

--- imports/api/events/events.test.js
+++ imports/api/events/events.test.js
@@ -3,17 +3,9 @@ import {assert} from "meteor/practicalmeteor:chai";
 import {faker} from "meteor/practicalmeteor:faker";
 import {resetDatabase} from "meteor/xolvio:cleaner";
 
-import { Events } from './events.js';
-import {
-    getAggregatedGraph,
-    getEventAncestorGraph, }
-    from './methods';
+import {Events} from "./events.js";
+import {getAggregatedGraph, getEventAncestorGraph} from "./methods";
 
-import {
-    isTestEvent,
-    isConfidenceLevelEvent,
-    isFinishedEvent}
-    from './eventTypes.js';
 
 if (Meteor.isServer) {
 
@@ -32,52 +24,24 @@ if (Meteor.isServer) {
     // Helper function for the factory creating dummy data
     function randomData() {
         let randomEiffelEvent = function () {
+            // Just a few of the many events
             let events = [
-                'ActI',
-                'ActC',
-                'ActS',
-                'ActF',
-                'ArtC',
-                'ArtP',
-                'ArtR',
-                'CLM',
                 'ED',
                 'CD',
-                'SCC',
-                'SCS',
-                'FCD',
-                'TCS',
-                'TCF',
+                'TFC',
                 'TSS',
-                'TSF',
                 'IV',
-                'TERCC',
-                'AnnP'
+                'CLM'
             ];
-            return events[_.random(0, events.length - 1)];
+            return events[Math.floor(Math.random() * events.length)];
         };
 
-        let event = randomEiffelEvent();
-        let data = {
+        return {
             customData: [{
-                value: event,
+                value: randomEiffelEvent(),
                 key: "name"
-            }]
-        };
-
-        if (isTestEvent(event)) {
-            let possibleVerdicts = ['PASSED', 'FAILED'];
-            data.outcome = {
-                verdict: possibleVerdicts[_.random(0, possibleVerdicts.length - 1)]
-            };
+            }],
         }
-
-        if (isConfidenceLevelEvent(event)) {
-            let possibleValues = ['PASSED', 'FAILED', 'INCONCLUSIVE'];
-            data.value = possibleValues[_.random(0, possibleValues.length - 1)]
-        }
-
-        return data;
     }
 
     describe('getAggregatedGraph', function () {
@@ -114,8 +78,7 @@ if (Meteor.isServer) {
 
             // Call function under test and extract the aggregated events
             let graph = getAggregatedGraph.call({from: from, to: to, limit: limit});
-
-            let aggregatedEvents = _.flatten(_.map(graph.nodes, (node) => node.data.events));
+            let aggregatedEvents = _.flatten(_.map(graph.nodes, (n) => n.data.events));
 
             // Every event should be in the time span
             assert(_.every(aggregatedEvents, (e) =>
@@ -158,125 +121,4 @@ if (Meteor.isServer) {
             assert.notEqual(graph, undefined);
         });
     });
-
-    describe('isTestEvent', function () {
-        it('returns true for events containing tests', function () {
-            let events = [
-                'EiffelTestCaseFinishedEvent',
-                'EiffelTestSuiteFinishedEvent'
-            ];
-
-            let result = _.every(events, isTestEvent);
-            assert.isTrue(result);
-        });
-
-
-        it('returns false for events not containing tests', function () {
-            let events = [
-                'EiffelActivityTriggeredEvent',
-                'EiffelActivityCanceledEvent',
-                'EiffelActivityStartedEvent',
-                'EiffelActivityFinishedEvent',
-                'EiffelArtifactCreatedEvent',
-                'EiffelArtifactPublishedEvent',
-                'EiffelArtifactReusedEvent',
-                'EiffelConfidenceLevelModifiedEvent',
-                'EiffelEnvironmentDefinedEvent',
-                'EiffelCompositionDefinedEvent',
-                'EiffelSourceChangeCreatedEvent',
-                'EiffelSourceChangeSubmittedEvent',
-                'EiffelFlowContextDefinedEvent',
-                'EiffelTestCaseTriggeredEvent',
-                'EiffelTestCaseCanceledEvent',
-                'EiffelTestCaseStartedEvent',
-                'EiffelTestCaseFinishedEvent',
-                'EiffelTestSuiteStartedEvent',
-                'EiffelTestSuiteFinishedEvent',
-                'EiffelIssueVerifiedEvent',
-                'EiffelTestExecutionRecipeCollectionCreatedEvent',
-                'EiffelAnnouncementPublishedEvent'
-            ];
-
-            let result = _.every(events, !isTestEvent);
-            assert.isTrue(result);
-        });
-    });
-
-    describe('isConfidenceLevelEvent', function () {
-        it('returns true for confidence level modified event', function () {
-            let event = 'EiffelConfidenceLevelModifiedEvent';
-            let result = isConfidenceLevelEvent(event);
-            assert.isTrue(result);
-        });
-
-        it('returns false for events not being confidence level changed events', function () {
-            let events = [
-                'EiffelActivityTriggeredEvent',
-                'EiffelActivityCanceledEvent',
-                'EiffelActivityStartedEvent',
-                'EiffelActivityFinishedEvent',
-                'EiffelArtifactCreatedEvent',
-                'EiffelArtifactPublishedEvent',
-                'EiffelArtifactReusedEvent',
-                'EiffelEnvironmentDefinedEvent',
-                'EiffelCompositionDefinedEvent',
-                'EiffelSourceChangeCreatedEvent',
-                'EiffelSourceChangeSubmittedEvent',
-                'EiffelFlowContextDefinedEvent',
-                'EiffelTestCaseTriggeredEvent',
-                'EiffelTestCaseCanceledEvent',
-                'EiffelTestCaseStartedEvent',
-                'EiffelTestCaseFinishedEvent',
-                'EiffelTestSuiteStartedEvent',
-                'EiffelTestSuiteFinishedEvent',
-                'EiffelIssueVerifiedEvent',
-                'EiffelTestExecutionRecipeCollectionCreatedEvent',
-                'EiffelAnnouncementPublishedEvent'
-            ];
-
-            let result = _.every(events, !isConfidenceLevelEvent);
-            assert.isTrue(result);
-        });
-    });
-
-
-    describe('isFinishedEvent', function () {
-        it('returns true for events that signal that something finished', function () {
-            let events = [
-                'EiffelActivityFinishedEvent',
-                'EiffelTestCaseFinishedEvent',
-                'EiffelTestSuiteFinishedEvent'
-            ];
-            let result = _.every(events, !isFinishedEvent);
-            assert.isTrue(result);
-        });
-
-        it('returns false for events not signaling that something finished', function () {
-            let events = [
-                'EiffelActivityTriggeredEvent',
-                'EiffelActivityCanceledEvent',
-                'EiffelActivityStartedEvent',
-                'EiffelArtifactCreatedEvent',
-                'EiffelArtifactPublishedEvent',
-                'EiffelArtifactReusedEvent',
-                'EiffelEnvironmentDefinedEvent',
-                'EiffelCompositionDefinedEvent',
-                'EiffelSourceChangeCreatedEvent',
-                'EiffelSourceChangeSubmittedEvent',
-                'EiffelFlowContextDefinedEvent',
-                'EiffelTestCaseTriggeredEvent',
-                'EiffelTestCaseCanceledEvent',
-                'EiffelTestCaseStartedEvent',
-                'EiffelTestSuiteStartedEvent',
-                'EiffelIssueVerifiedEvent',
-                'EiffelTestExecutionRecipeCollectionCreatedEvent',
-                'EiffelAnnouncementPublishedEvent'
-            ];
-
-            let result = _.every(events, !isFinishedEvent);
-            assert.isTrue(result);
-        });
-    });
 }
-
-
