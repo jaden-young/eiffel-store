9ccac981e268b54750e4fdc5a27db53c147f7d6b
Joakim Argillander <joaar286@student.liu.se>
2017-04-28 17:05:13 +0200

    Edited title of BS-modal

--- imports/ui/layout/layout.js
+++ imports/ui/layout/layout.js
@@ -108,21 +108,21 @@ Meteor.autorun(function(){
         if( !hasShownConnectionInfo && !(Meteor.status().status === "connected")) {
             $('#lost-connection-modal').modal('show');
             hasShownConnectionInfo = true;
-            var reason;
+            let reason;
 
             if(Meteor.status.reason === "undefined"){
-                "<i>Could not resolve reason. You have probably lost your network connection. </i>"
+                reason = "<i>Could not resolve reason. You have probably lost your network connection. </i>"
             }else{
                 reason = Meteor.status.reason;
             }
             $('#connectionStatus').html('<a href="#" data-toggle="modal" data-target="#lost-connection-modal"><span class="glyphicon glyphicon-ban-circle" aria-hidden="true" style = "color:red"></span></a>');
-            $('#lost-connection-modal-body').html("<p> Connection to server lost! Please check your network settings and try again. <br> <br> " + reason + "</p>");
-            $('#lost-connection-modal-title').html('<span class="glyphicon glyphicon-remove-circle" aria-hidden="true" style="color:red"></span> Connection error');
+            $('#lost_connection_modal_body').html("<p> Connection to server lost! Please check your network settings and try again. <br> <br> " + reason + "</p>");
+            $('#lost_connection_modal_title').html('<span class="glyphicon glyphicon-remove-circle" aria-hidden="true" style="color:red"></span> Connection error');
         }else if(Meteor.status().status === "connected"){
             hasShownConnectionInfo = false;
             $('#connectionStatus').html('<a href="#" data-toggle="modal" data-target="#lost-connection-modal"><span class="glyphicon glyphicon-ok-sign" aria-hidden="true" style = "color:green"></span></a>');
-            $('#lost-connection-modal-body').html("<p> You are connected to the server. </p>");
-            $('#lost-connection-modal-title').html('<span class="glyphicon glyphicon-ok-circle" aria-hidden="true" style="color:green"></span> Connection to server');
+            $('#lost_connection_modal_body').html("<p> You are connected to the server. </p>");
+            $('#lost_connection_modal_title').html('<span class="glyphicon glyphicon-ok-circle" aria-hidden="true" style="color:green"></span> Connection to server');
         }
     }
 );
