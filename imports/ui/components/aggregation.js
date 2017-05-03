09b5e6185896fe9662bb05ba6acfc1530be5cf3a
Sebastian <sebastian.callh@gmail.com>
2017-05-03 15:48:50 +0200

    fixed errors in additional sequences again

--- imports/ui/components/aggregation.js
+++ imports/ui/components/aggregation.js
@@ -186,7 +186,7 @@ function showAggregation(from, to, limit) {
             let container = $('#cy-aggregation');
             Session.set('displayedSequenceIds', graph.sequences);
             renderGraph(graph, container, 'aggregation');
-            showSequenceCount(from, to, limit);
+            showSequenceCount(from, to, graph.sequences.length);
             show(3);
         }
     });
