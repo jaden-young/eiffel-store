4d9408afd798ffff387a9a3148e9a45884fc35a1
becczor <rebecca.lindblom@gmail.com>
2017-05-17 21:34:28 +0200

    Flag for activity now correct and node types sorted alphabetically

--- imports/ui/components/graph.js
+++ imports/ui/components/graph.js
@@ -62,7 +62,75 @@ function renderGraph(graph, container, level) {
             },
 
             {
-                selector: 'node[label ^= "CLM"]', // All nodes with ID == CLM (Confidence level)
+                selector: 'node[id ^= "Act"]', // All nodes with ID Activity
+                style: {
+                    'shape': 'polygon',
+                    'shape-polygon-points': '-0.95 -0.77 -0.9 -0.82 -0.85 -0.87 -0.8 -0.91 -0.74 -0.94 -0.68 -0.97 -0.62 -0.98 -0.56 -1 -0.5 -1 -0.44 -1 -0.38 -0.98 -0.32 -0.97 -0.26 -0.94 -0.2 -0.91 -0.15 -0.87 -0.1 -0.82 -0.05 -0.77 0.05 -0.67 0.1 -0.62 0.15 -0.57 0.2 -0.53 0.26 -0.5 0.32 -0.47 0.38 -0.46 0.44 -0.44 0.5 -0.44 0.56 -0.44 0.62 -0.46 0.68 -0.47 0.74 -0.5 0.8 -0.53 0.85 -0.57 0.9 -0.62 0.95 -0.67 0.95 0.77 0.9 0.82 0.85 0.87 0.8 0.91 0.74 0.94 0.68 0.97 0.62 0.98 0.56 1 0.5 1 0.44 1 0.38 0.98 0.32 0.97 0.26 0.94 0.2 0.91 0.15 0.87 0.1 0.82 0.05 0.77 -0.05 0.67 -0.1 0.62 -0.15 0.57 -0.2 0.53 -0.26 0.5 -0.32 0.47 -0.38 0.46 -0.44 0.44 -0.5 0.44 -0.56 0.44 -0.62 0.46 -0.68 0.47 -0.74 0.5 -0.8 0.53 -0.85 0.57 -0.9 0.62 -0.95 0.67',
+                    'height': 60,
+                    'width': 100,
+                    'background-color': FAIL_COLOR,
+                    'background-position-x': '0px',
+                    'background-image': '/images/green.png',
+                    'background-height': '100%',
+                    'background-width': function (ele) {
+                        return (ele.data("successful") * 100 / ele.data("length") ).toString() + '%';
+                    }
+                }
+            },
+
+            {
+                selector: 'node[id ^= "AP"]', // All nodes with ID Announcement Published
+                style: {
+                    'shape': 'polygon',
+                    'shape-polygon-points': '-0.1 0.99 -0.27 0.96 -0.43 0.9 -0.58 0.81 -0.72 0.7 -0.83 0.56 -0.91 0.41 -0.97 0.25 -1 0.07 -0.99 -0.1 -0.96 -0.27 -0.9 -0.43 -0.81 -0.58 -0.7 -0.72 -0.56 -0.83 -0.41 -0.91 -0.25 -0.97 -0.07 -1 0.1 -0.99 0.27 -0.96 0.43 -0.9 0.58 -0.81 0.72 -0.7 0.83 -0.56 0.91 -0.41 0.97 -0.25 1 -0.07 1 0 0.98 0.17 0.94 0.34 0.87 0.5 0.77 0.64 0.64 0.77 0.5 0.87 0.34 0.94 0.17 0.98 0 1 -0.33 0.07 0.51 0.16 0.45 0.1 0.4 0.05 0.29 -0.16 0.22 -0.3 0.19 -0.42 0.17 -0.5 0.16 -0.6 0.17 -0.7 0.18 -0.78 0.26 -0.7 0.3 -0.66 0.35 -0.6 0.41 -0.5 0.47 -0.4 0.49 -0.3 0.52 -0.2 0.53 -0.1 0.54 0 0.52 0.1 0.51 0.16 0.45 0.1 0.4 0.05 0.29 -0.16 0.22 -0.3 0.19 -0.42 0.17 -0.5 0.16 -0.6 0.17 -0.7 0.18 -0.78 -0.46 -0.09 -0.61 -0.05 -0.57 0.13 -0.41 0.09',
+                    'height': 90,
+                    'width': 90,
+                }
+            },
+
+            {
+                selector: 'node[id ^= "Art"]', // All nodes with ID starting with Art (Artifact)
+                style: {
+                    'shape': 'polygon',
+                    'shape-polygon-points': '1 -0.4 0 -0.8 -1 -0.4 0 0 1 -0.4 1 0.6 0 1 0 0 0 1 -1 0.6 -1 -0.4 0 0 1 -0.4',
+                    'height': 60,
+                    'width': 50,
+                }
+            },
+
+            {
+                selector: 'node[id ^= "ArtC"]', // All nodes with ID Artifact Created
+                style: {
+                    'background-color': '#557e62',
+                }
+            },
+
+            {
+                selector: 'node[id ^= "ArtP"]', // All nodes with ID Artifact Published
+                style: {
+                    'background-color': '#5a617e',
+                }
+            },
+
+            {
+                selector: 'node[id ^= "ArtR"]', // All nodes with ID Artifact Reused
+                style: {
+                    'background-color': '#7e5344',
+                }
+            },
+
+            {
+                selector: 'node[id ^= "CDef"]', // All nodes with ID Composition Defined
+                style: {
+                    'shape': 'polygon',
+                    'shape-polygon-points': '1 0 1 0.6 0.5 0.8 0 0.6 -0.5 0.8 -1 0.6 -1 0 -0.5 -0.2 -0.5 -0.8 0 -1 0.5 -0.8 0.5 -0.2 1 0  0.5 0.2 0.5 0.8 0.5 0.2 0 0 0 0.6 0 0 -0.5 0.2 -0.5 0.8 -0.5 0.2 -1 0 -0.5 -0.2 0 0 0.5 -0.2 0 0 0 -0.6 -0.5 -0.8 0 -0.6 0.5 -0.8 0.5 -0.2 1 0',
+                    'height': 70,
+                    'width': 70,
+                }
+            },
+
+            {
+                selector: 'node[label ^= "CLM"]', // All nodes with ID Confidence Level
                 style: {
                     'background-color': '#fff',
                     'width': '70px',
@@ -84,7 +152,41 @@ function renderGraph(graph, container, level) {
             },
 
             {
-                selector: 'node[label ^= "TC"]', // All nodes with ID starting with TC(Test Case)
+                selector: 'node[id ^= "CA"]', // All nodes with ID Configuration Applied
+                style: {
+                    'shape': 'polygon',
+                    'shape-polygon-points': '-0.1 1 -0.17 0.77 -0.32 0.72 -0.53 0.87 -0.68 0.77 -0.6 0.53 -0.68 0.38 -0.94 0.39 -1 0.22 -0.79 0.08 -0.79 -0.08 -1 -0.22 -0.94 -0.39 -0.68 -0.38 -0.6 -0.53 -0.68 -0.77 -0.53 -0.87 -0.32 -0.72 -0.17 -0.77 -0.1 -1 0.1 -1 0.17 -0.77 0.32 -0.72 0.53 -0.87 0.68 -0.77 0.6 -0.53 0.68 -0.38 0.94 -0.39 1 -0.22 0.79 -0.08 0.79 0.08 1 0.22 0.94 0.39 0.68 0.38 0.6 0.53 0.68 0.77 0.53 0.87 0.32 0.72 0.17 0.77 0.1 1',
+                    'height': 70,
+                    'width': 70,
+                    'pie-size': '40%',
+                    'pie-1-background-size': '100%',
+                    'pie-1-background-color': '#fff',
+                }
+            },
+
+            {
+                selector: 'node[id ^= "EDef"]', // All nodes with ID Environment Defined
+                style: {
+                    'background-color': '#ff7e37',
+                    'shape': 'polygon',
+                    'shape-polygon-points': '-0.2 0.98 -0.39 0.92 -0.56 0.83 -0.71 0.7 -0.83 0.55 -0.92 0.38 -0.98 0.19 -1 0 -0.98 -0.2 -0.92 -0.38 -0.83 -0.56 -0.71 -0.71 -0.55 -0.83 -0.38 -0.92 -0.19 -0.98 0 -1 0.19 -0.98 0.38 -0.92 0.55 -0.83 0.71 -0.71 0.83 -0.56 0.92 -0.38 0.98 -0.2 1 0 1 0 0.98 0.19 0.92 0.38 0.83 0.55 0.71 0.71 0.56 0.83 0.38 0.92 0.2 0.98 0.2 0.1 0.5 0.1 0 -0.5 -0.5 0.1 -0.2 0.1',
+                    'height': 70,
+                    'width': 70,
+                }
+            },
+
+            {
+                selector: 'node[id ^= "SCC"]', // All nodes with ID Source Change Created
+                style: {
+                    'shape': 'polygon',
+                    'shape-polygon-points': '-0.91 0.9 -0.93 0.91 -0.94 0.91 -0.94 0.93 -0.95 0.94 -0.95 0.95 -0.95 0.96 -0.94 0.98 -0.94 0.99 -0.93 0.99 -0.91 1 -0.9 1 -0.89 1 -0.88 0.99 -0.86 0.99 -0.86 0.98 -0.85 0.96 -0.85 0.95 -0.85 0.95 -0.85 0.94 -0.86 0.93 -0.86 0.91 -0.88 0.91 -0.89 0.9 -0.89 0.55 0.88 0.04 0.89 0.05 0.9 0.05 0.91 0.05 0.93 0.04 0.94 0.04 0.94 0.03 0.95 0.01 0.95 0 0.95 0 0.95 -0.01 0.94 -0.03 0.94 -0.04 0.93 -0.04 0.91 -0.05 0.9 -0.05 0.89 -0.05 0.88 -0.04 0.86 -0.04 0.86 -0.03 0.85 -0.01 0.85 0 -0.89 0.5 -0.89 -0.9 -0.81 -0.9 -0.9 -1 -0.99 -0.9 -0.91 -0.9',
+                    'height': 70,
+                    'width': 70,
+                }
+            },
+
+            {
+                selector: 'node[label ^= "TC"]', // All nodes with ID Test Case
                 style: {
                     'background-color': FAIL_COLOR,
                     'shape': 'rectangle',
@@ -98,8 +200,9 @@ function renderGraph(graph, container, level) {
                     'background-position-x': '0px'
                 }
             },
+
             {
-                selector: 'node[label ^= "TS"]', // All nodes with ID starting with TSF(Test Suite Finished)
+                selector: 'node[label ^= "TS"]', // All nodes with ID Test Suite
                 style: {
                     'shape': 'rectangle',
                     'border-style': 'double', // solid, dotted, dashed, or double.
@@ -116,107 +219,27 @@ function renderGraph(graph, container, level) {
                 }
             },
 
-            {
-                selector: 'node[id ^= "CA"]', // All nodes with ID starting with CD (Composition Defined)
-                style: {
-                    'shape': 'polygon',
-                    'shape-polygon-points': '-0.1 1 -0.17 0.77 -0.32 0.72 -0.53 0.87 -0.68 0.77 -0.6 0.53 -0.68 0.38 -0.94 0.39 -1 0.22 -0.79 0.08 -0.79 -0.08 -1 -0.22 -0.94 -0.39 -0.68 -0.38 -0.6 -0.53 -0.68 -0.77 -0.53 -0.87 -0.32 -0.72 -0.17 -0.77 -0.1 -1 0.1 -1 0.17 -0.77 0.32 -0.72 0.53 -0.87 0.68 -0.77 0.6 -0.53 0.68 -0.38 0.94 -0.39 1 -0.22 0.79 -0.08 0.79 0.08 1 0.22 0.94 0.39 0.68 0.38 0.6 0.53 0.68 0.77 0.53 0.87 0.32 0.72 0.17 0.77 0.1 1',
-                    'height': 70,
-                    'width': 70,
-                    'pie-size': '40%',
-                    'pie-1-background-size': '100%',
-                    'pie-1-background-color': '#fff',
-                }
-            },
+            
+
+            
+
+            
+
+            
+
+            
+
+            
 
             {
-                selector: 'node[id ^= "Act"]', // All nodes with ID starting with Act (Activity)
-                style: {
-                    'shape': 'polygon',
-                    'shape-polygon-points': '-0.43 -0.9 -0.41 -0.92 -0.38 -0.94 -0.36 -0.96 -0.33 -0.97 -0.31 -0.98 -0.28 -0.99 -0.26 -1 -0.23 -1 -0.2 -1 -0.18 -1 -0.15 -0.99 -0.13 -0.98 -0.1 -0.97 -0.08 -0.96 -0.05 -0.94 -0.03 -0.92 0 -0.9 0.43 -0.9 0.41 -0.88 0.38 -0.86 0.36 -0.84 0.33 -0.83 0.31 -0.82 0.28 -0.81 0.26 -0.8 0.23 -0.8 0.2 -0.8 0.18 -0.8 0.15 -0.81 0.13 -0.82 0.1 -0.83 0.08 -0.84 0.05 -0.86 0.03 -0.88 0 -0.9 0.43 -0.1 0.41 -0.08 0.38 -0.06 0.36 -0.04 0.33 -0.03 0.31 -0.02 0.28 -0.01 0.26 0 0.23 0 0.2 0 0.18 0 0.15 -0.01 0.13 -0.02 0.1 -0.03 0.08 -0.04 0.05 -0.06 0.03 -0.08 0 -0.1 -0.43 -0.1 -0.41 -0.12 -0.38 -0.14 -0.36 -0.16 -0.33 -0.17 -0.31 -0.18 -0.28 -0.19 -0.26 -0.2 -0.23 -0.2 -0.2 -0.2 -0.18 -0.2 -0.15 -0.19 -0.13 -0.18 -0.1 -0.17 -0.08 -0.16 -0.05 -0.14 -0.03 -0.12 0 -0.1',
-                    'height': 40,
-                    'width': 40,
-                    'background-color': FAIL_COLOR,
-                    'background-position-x': '0px',
-                    'background-image': '/images/green.png',
-                    'background-height': '100%',
-                    'background-width': function (ele) {
-                        return (ele.data("successful") * 100 / ele.data("length") ).toString() + '%';
-                    }
-                }
-            },
-
-            {
-                selector: 'node[id ^= "Art"]', // All nodes with ID starting with Act (Activity)
-                style: {
-                    'shape': 'polygon',
-                    'shape-polygon-points': '1 -0.4 0 -0.8 -1 -0.4 0 0 1 -0.4 1 0.6 0 1 0 0 0 1 -1 0.6 -1 -0.4 0 0 1 -0.4',
-                    'height': 60,
-                    'width': 50,
-                }
-            },
-
-            {
-                selector: 'node[id ^= "ArtC"]', // All nodes with ID starting with Act (Activity)
-                style: {
-                    'background-color': '#557e62',
-                }
-            },
-
-            {
-                selector: 'node[id ^= "ArtP"]', // All nodes with ID starting with Act (Activity)
-                style: {
-                    'background-color': '#5a617e',
-                }
-            },
-
-            {
-                selector: 'node[id ^= "ArtR"]', // All nodes with ID starting with Act (Activity)
-                style: {
-                    'background-color': '#7e5344',
-                }
-            },
-
-            {
-                selector: 'node[id ^= "IssV"]', // All nodes with ID Sourcs Change Submitted
-                style: {
-                    'background-color': '#ff7e37',
-                    'shape': 'polygon',
-                    'shape-polygon-points': '-0.2 0.98 -0.39 0.92 -0.56 0.83 -0.71 0.7 -0.83 0.55 -0.92 0.38 -0.98 0.19 -1 0 -0.98 -0.2 -0.92 -0.38 -0.83 -0.56 -0.71 -0.71 -0.55 -0.83 -0.38 -0.92 -0.19 -0.98 0 -1 0.19 -0.98 0.38 -0.92 0.55 -0.83 0.71 -0.71 0.83 -0.56 0.92 -0.38 0.98 -0.2 1 0 1 0 0.98 0.19 0.92 0.38 0.83 0.55 0.71 0.71 0.56 0.83 0.38 0.92 0.2 0.98 0.2 0.1 0.5 0.1 0 -0.5 -0.5 0.1 -0.2 0.1',
-                    'height': 70,
-                    'width': 70,
-                }
-            },
-
-            {
-                selector: 'node[id ^= "CDef"]', // All nodes with ID Sourcs Change Submitted
-                style: {
-                    'shape': 'polygon',
-                    'shape-polygon-points': '1 0 1 0.6 0.5 0.8 0 0.6 -0.5 0.8 -1 0.6 -1 0 -0.5 -0.2 -0.5 -0.8 0 -1 0.5 -0.8 0.5 -0.2 1 0  0.5 0.2 0.5 0.8 0.5 0.2 0 0 0 0.6 0 0 -0.5 0.2 -0.5 0.8 -0.5 0.2 -1 0 -0.5 -0.2 0 0 0.5 -0.2 0 0 0 -0.6 -0.5 -0.8 0 -0.6 0.5 -0.8 0.5 -0.2 1 0',
-                    'height': 90,
-                    'width': 90,
-                }
-            },
-
-            {
-                selector: 'node[id ^= "EDef"]', // All nodes with ID Sourcs Change Submitted
-                style: {
-                    'shape': 'polygon',
-                    'shape-polygon-points': '-0.1 0.99 -0.27 0.96 -0.43 0.9 -0.58 0.81 -0.72 0.7 -0.83 0.56 -0.91 0.41 -0.97 0.25 -1 0.07 -0.99 -0.1 -0.96 -0.27 -0.9 -0.43 -0.81 -0.58 -0.7 -0.72 -0.56 -0.83 -0.41 -0.91 -0.25 -0.97 -0.07 -1 0.1 -0.99 0.27 -0.96 0.43 -0.9 0.58 -0.81 0.72 -0.7 0.83 -0.56 0.91 -0.41 0.97 -0.25 1 -0.07 1 0 0.98 0.17 0.94 0.34 0.87 0.5 0.77 0.64 0.64 0.77 0.5 0.87 0.34 0.94 0.17 0.98 0 1 -0.33 0.07 0.51 0.16 0.45 0.1 0.4 0.05 0.29 -0.16 0.22 -0.3 0.19 -0.42 0.17 -0.5 0.16 -0.6 0.17 -0.7 0.18 -0.78 0.26 -0.7 0.3 -0.66 0.35 -0.6 0.41 -0.5 0.47 -0.4 0.49 -0.3 0.52 -0.2 0.53 -0.1 0.54 0 0.52 0.1 0.51 0.16 0.45 0.1 0.4 0.05 0.29 -0.16 0.22 -0.3 0.19 -0.42 0.17 -0.5 0.16 -0.6 0.17 -0.7 0.18 -0.78 -0.46 -0.09 -0.61 -0.05 -0.57 0.13 -0.41 0.09',
-                    'height': 90,
-                    'width': 90,
-                }
-            },
-
-            {
-                selector: 'node[extra = "highlight"]', // All nodes with ID starting with TSF(Test Suite Finished)
+                selector: 'node[extra = "highlight"]', // Clicked node från navigation from table to event chain
                 style: {
                     'border-width': '8px', // The size of the node’s border.
                     'border-color': '#ffea22',
                 }
             },
             {
-                selector: 'node[extra = "hidden"]', // All nodes with ID starting with TSF(Test Suite Finished)
+                selector: 'node[extra = "hidden"]', // All nodes that should be hidden
                 style: {
                     'opacity': 0
                 }
