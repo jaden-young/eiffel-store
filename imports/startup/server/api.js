c1eaf22868cc6c3ecffec76656a42a9ec2e78692
Jonathan Wahlund <jonwa732@student.liu.se>
2017-05-18 12:55:36 +0200

    Removed unused code

--- imports/startup/server/api.js
+++ imports/startup/server/api.js
@@ -3,7 +3,3 @@ import "../../api/initializer/methods";
 import "../../api/eiffelevents/methods";
 import "../../api/events/methods";
 import "../../api/rows/methods";
-
-//Commented out to force-fail server connection check
-import "../../api/utils/methods"
-
